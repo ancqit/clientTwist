eec30e4a66ce35905fa13969f8a0b5e2a49f43a1
ancqit <ancqit@gmail.com>
2025-06-01 17:18:08 +0530

    added languages to translations

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from './Services/translate-service'; // Import the service
 
@@ -9,14 +9,28 @@ import { TranslateService } from './Services/translate-service'; // Import the s
   styleUrl: './app.scss'
 })
 export class App {
+  showChild: boolean = true;
+
   /**
    * Initializes the App component and loads translations at startup.
    * 
    * @param translateService - Service responsible for handling translations and caching them.
    */
-  constructor(private translateService: TranslateService) {
-    this.translateService.loadAndCacheTranslations(); // Load translations at app startup
+  constructor(private translateService: TranslateService,
+    private cdr: ChangeDetectorRef) {
+    this.translateService.loadAndCacheTranslations();
+    // Load translations at app startup
   }
+  changeLanguage(arg0: string) {
+    this.translateService.currentLanguage(arg0);
+    this.translateService.loadAndCacheTranslations();
+    this.showChild = false;
+    setTimeout(() => {
+      this.showChild = true;
+      this.cdr.detectChanges(); // Manually trigger change detection
+    }, 500);
+  }
+
   // This is the main application component.
   // It serves as the root component for the Angular application.
   // The RouterOutlet directive is used to display routed components.
